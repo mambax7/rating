aa95b488c4948ea8007554613905d001dfb0c608
mambax7 <mambax7@gmail.com>
2021-11-01 02:14:08 -0400

    Cascading dirname(...) calls

--- class/Common/Configurator.php
+++ class/Common/Configurator.php
@@ -43,7 +43,7 @@ class Configurator
         $moduleDirName      = \basename(\dirname(__DIR__, 2));
         $moduleDirNameUpper = mb_strtoupper($moduleDirName);
 
-        $config = require \dirname(\dirname(__DIR__)) . '/config/config.php';
+        $config = require dirname(__DIR__, 2) . '/config/config.php';
 
         $this->name            = $config->name;
         $this->paths           = $config->paths;
